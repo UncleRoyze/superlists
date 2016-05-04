8bbf6a63d10221f3ea88bc04b56dd9ba6df512ae
Roy Chuang <chuangch.csie@gmail.com>
2016-05-04 14:09:57 +0800

    Add FT for different users can only edit their own lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,36 +6,57 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
-    	self.browser = webdriver.Firefox()
-    	self.browser.implicitly_wait(3)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
-    	self.browser.quit()
+        self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-    	table = self.browser.find_element_by_id('id_list_table')
-    	rows = table.find_elements_by_tag_name('tr')
-    	self.assertIn(row_text, [row.text for row in rows])
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-    	self.browser.get(self.live_server_url)
-    	self.assertIn('To-Do', self.browser.title)
+        self.browser.get(self.live_server_url)
+        self.assertIn('To-Do', self.browser.title)
 
-    	header_text = self.browser.find_element_by_tag_name('h1').text
-    	self.assertIn('To-Do', header_text)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-    	inputbox = self.browser.find_element_by_id('id_new_item')
-    	self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-    	inputbox.send_keys('Kiss Chien Mien Mien')
-    	inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('Kiss Chien Mien Mien')
+        inputbox.send_keys(Keys.ENTER)
 
-    	self.check_for_row_in_list_table('1: Kiss Chien Mien Mien')
+        roy_list_url = self.browser.current_url
+        self.assertRegex(roy_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Kiss Chien Mien Mien')
 
-    	inputbox = self.browser.find_element_by_id('id_new_item')
-    	inputbox.send_keys('Kiss Chien Mien Mien again!')
-    	inputbox.send_keys(Keys.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Kiss Chien Mien Mien again!')
+        inputbox.send_keys(Keys.ENTER)
 
-    	self.check_for_row_in_list_table('2: Kiss Chien Mien Mien again!')
+        self.check_for_row_in_list_table('2: Kiss Chien Mien Mien again!')
 
-    	self.fail('Finish the test!')
+        ## We use a new browser session to make sure that no information
+        ## of Roy's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kiss Chien Mien Mien', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Kiss Roy')
+        inputbox.send_keys(Keys.ENTER)
+
+        mien_list_url = self.browser.current_url
+        self.assertRegex(mien_list_url, 'lists/.+')
+        self.assertNotEqual(roy_list_url, mien_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kiss Chien Mien Mien', page_text)
+        self.assertIn('Kiss Roy', page_text)
